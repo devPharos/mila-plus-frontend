cb48537369b14057810e54e5e7585b2429362949
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-10-24 19:06:04 -0300

    Refatorar importações no componente Outside.js

--- src/pages/Outside.js
+++ src/pages/Outside.js
@@ -1,13 +1,13 @@
-import React, { useContext } from 'react';
-import { Outlet } from 'react-router-dom';
-import { PageContext } from '~/App';
+import React, { useContext } from "react";
+import { Outlet } from "react-router-dom";
+import { PageContext } from "~/App";
 
 export default function Outside() {
+  const { pages } = useContext(PageContext);
 
-    const { pages } = useContext(PageContext)
-
-    return <div className='w-full bg-gradient-to-br from-gray-300 via-indigo-300 to-mila_orange flex flex-1 flex-row justify-between items-center px-4 pt-8 shadow-lg overflow-y-scroll'>
-
-        <Outlet />
+  return (
+    <div className="w-full bg-gradient-to-br from-gray-300 via-indigo-300 to-mila_orange flex flex-1 flex-row justify-between items-center px-4 pt-8 shadow-lg overflow-y-scroll">
+      <Outlet />
     </div>
+  );
 }

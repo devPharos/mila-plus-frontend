1d7f959f99aa222f5fb5661146e6f199c695f1b4
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-10-24 21:09:05 -0300

    Refatorar campos de data e adicionar novo componente FinancialBankAccount

---
+++ src/pages/Financial/BankAccount/Preview/index.js
@@ -0,0 +1,307 @@
+import { Form } from "@unform/web";
+import { Building, Pencil, X } from "lucide-react";
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import Input from "~/components/RegisterForm/Input";
+import RegisterFormMenu from "~/components/RegisterForm/Menu";
+import InputLine from "~/components/RegisterForm/InputLine";
+import InputLineGroup from "~/components/RegisterForm/InputLineGroup";
+import FormHeader from "~/components/RegisterForm/FormHeader";
+import Preview from "~/components/Preview";
+import { Zoom, toast } from "react-toastify";
+import api from "~/services/api";
+import { getRegistries, handleUpdatedFields } from "~/functions";
+import SelectPopover from "~/components/RegisterForm/SelectPopover";
+import FormLoading from "~/components/RegisterForm/FormLoading";
+
+export const InputContext = createContext({});
+
+export default function PagePreview({
+  access,
+  id,
+  handleOpened,
+  setOpened,
+  defaultFormType = "preview",
+  successfullyUpdated,
+  setSuccessfullyUpdated,
+}) {
+  // bank.bank_name, company.name, account, routing_number
+  const [pageData, setPageData] = useState({
+    loaded: false,
+    account: "",
+    routing_number: "",
+    bank_id: null,
+    bank: {
+      bank_name: "",
+    },
+    filial_id: null,
+    filial: {
+      name: "",
+    },
+  });
+
+  const [registry, setRegistry] = useState({
+    created_by: null,
+    created_at: null,
+    updated_by: null,
+    updated_at: null,
+    canceled_by: null,
+    canceled_at: null,
+  });
+  const [formType, setFormType] = useState(defaultFormType);
+  const [fullscreen, setFullscreen] = useState(false);
+  const [activeMenu, setActiveMenu] = useState("general");
+  const [filialOptions, setFilialOptions] = useState([]);
+  const [bankOptions, setBankOptions] = useState([]);
+
+  const generalForm = useRef();
+
+  function handleCloseForm() {
+    if (!successfullyUpdated) {
+      toast("Changes discarted!", { autoClose: 1000 });
+    }
+    handleOpened(null);
+  }
+
+  async function handleGeneralFormSubmit(data) {
+    if (successfullyUpdated) {
+      toast("No need to be saved!", {
+        autoClose: 1000,
+        type: "info",
+        transition: Zoom,
+      });
+      return;
+    }
+    if (id === "new") {
+      try {
+        const response = await api.post(`/bankaccounts`, data);
+        setOpened(response.data.id);
+        setPageData({ ...pageData, ...data });
+        setSuccessfullyUpdated(true);
+        toast("Saved!", { autoClose: 1000 });
+        handleOpened(null);
+      } catch (err) {
+        toast(err.response.data.error, { type: "error", autoClose: 3000 });
+      }
+    } else if (id !== "new") {
+      const updated = handleUpdatedFields(data, pageData);
+
+      if (updated.length > 0) {
+        const objUpdated = Object.fromEntries(updated);
+        try {
+          console.log(objUpdated);
+          await api.put(`/bankaccounts/${id}`, objUpdated);
+          setPageData({ ...pageData, ...objUpdated });
+          setSuccessfullyUpdated(true);
+          toast("Saved!", { autoClose: 1000 });
+          handleOpened(null);
+        } catch (err) {
+          toast(err.response.data.error, { type: "error", autoClose: 3000 });
+        }
+      } else {
+        console.log(updated);
+      }
+    }
+  }
+
+  useEffect(() => {
+    async function getPageData() {
+      try {
+        const { data } = await api.get(`bankaccounts/${id}`);
+        setPageData({ ...data, loaded: true });
+        const {
+          created_by,
+          created_at,
+          updated_by,
+          updated_at,
+          canceled_by,
+          canceled_at,
+        } = data;
+        const registries = await getRegistries({
+          created_by,
+          created_at,
+          updated_by,
+          updated_at,
+          canceled_by,
+          canceled_at,
+        });
+        setRegistry(registries);
+      } catch (err) {
+        toast(err.response.data.error, { type: "error", autoClose: 3000 });
+      }
+    }
+    async function getDefaultOptions() {
+      try {
+        const { data } = await api.get(`/filials`);
+        const filialOptions = data
+          .filter((f) => f.id !== id)
+          .map((f) => {
+            return { value: f.id, label: f.name };
+          });
+
+          setFilialOptions(filialOptions);
+      } catch (err) {
+        toast(err.response.data.error, { type: "error", autoClose: 3000 });
+      }
+
+      try {
+        const { data } = await api.get(`/bank`);
+        const bankOptions = data
+          .filter((f) => f.id !== id)
+          .map((f) => {
+            return { value: f.id, label: f.bank_name };
+          });
+
+        setBankOptions(bankOptions);
+      } catch (err) {
+        toast(err.response.data.error, { type: "error", autoClose: 3000 });
+      }
+    }
+    getDefaultOptions();
+    if (id === "new") {
+      setFormType("full");
+    } else if (id) {
+      getPageData();
+    }
+  }, []);
+
+  return (
+    <Preview formType={formType} fullscreen={fullscreen}>
+      {pageData ? (
+        formType === "preview" ? (
+          <div className="border h-full rounded-xl overflow-hidden flex flex-col justify-start gap-1 overflow-y-scroll">
+            <div className="relative bg-gray-100 h-16 px-4 py-2 flex flex-row items-start justify-start">
+              <button
+                onClick={() => setFormType("full")}
+                className="absolute top-2 right-20 text-md font-bold bg-mila_orange text-white rounded-md p-1 px-2 h-6 flex flex-row items-center justify-center text-xs gap-1"
+              >
+                <Pencil size={16} color="#fff" /> Open
+              </button>
+              <button
+                onClick={() => handleOpened(null)}
+                className="absolute top-2 right-2 text-md font-bold bg-secondary rounded-md p-1 px-2 h-6 flex flex-row items-center justify-center text-xs gap-1"
+              >
+                <X size={16} /> Close
+              </button>
+              <h2 style={{ fontSize: 24 }}>{pageData.name}</h2>
+            </div>
+          </div>
+        ) : (
+          <div className="flex h-full flex-row items-start justify-between gap-4">
+            <div className="flex flex-col items-center justify-between text-xs w-32 gap-4">
+              <RegisterFormMenu
+                setActiveMenu={setActiveMenu}
+                activeMenu={activeMenu}
+                name="general"
+              >
+                <Building size={16} /> General
+              </RegisterFormMenu>
+            </div>
+            <div className="border h-full rounded-xl overflow-hidden flex flex-1 flex-col justify-start">
+              <div className="flex flex-col items-start justify-start text-sm overflow-y-scroll">
+                <Form
+                  ref={generalForm}
+                  onSubmit={handleGeneralFormSubmit}
+                  className="w-full"
+                >
+                  <InputContext.Provider
+                    value={{
+                      id,
+                      generalForm,
+                      setSuccessfullyUpdated,
+                      fullscreen,
+                      setFullscreen,
+                      successfullyUpdated,
+                      handleCloseForm,
+                    }}
+                  >
+                    {id === "new" || pageData.loaded ? (
+                      <>
+                        <FormHeader
+                          access={access}
+                          title={pageData.name}
+                          registry={registry}
+                          InputContext={InputContext}
+                        />
+
+                        <InputLineGroup
+                          title="GENERAL"
+                          activeMenu={activeMenu === "general"}
+                        >
+                          <InputLine title="General Data">
+                            <Input
+                              type="number"
+                              name="account"
+                              required
+                              title="Bank Account"
+                              grow
+                              defaultValue={pageData.account}
+                              InputContext={InputContext}
+                            />
+                            <Input
+                              type="number"
+                              name="routing_number"
+                              required
+                              title="Routing Number"
+                              grow
+                              defaultValue={pageData.routing_number}
+                              InputContext={InputContext}
+                            />
+                            <SelectPopover
+                              type="text"
+                              isSearchable
+                              name="bank_id"
+                              required
+                              title="Bank"
+                              options={bankOptions}
+                              grow
+                              defaultValue={
+                                pageData.bank_id
+                                  ? {
+                                      value: pageData.bank_id,
+                                      label: pageData.bank.bank_name,
+                                    }
+                                  : null
+                              }
+                              InputContext={InputContext}
+                            />
+                            <SelectPopover
+                              type="text"
+                              isSearchable
+                              name="filial_id"
+                              required
+                              title="Filial"
+                              options={filialOptions}
+                              grow
+                              defaultValue={
+                                pageData.filial_id
+                                  ? {
+                                      value: pageData.filial_id,
+                                      label: pageData.filial.name,
+                                    }
+                                  : null
+                              }
+                              InputContext={InputContext}
+                            />
+
+                          </InputLine>
+                        </InputLineGroup>
+                      </>
+                    ) : (
+                      <FormLoading />
+                    )}
+                  </InputContext.Provider>
+                </Form>
+              </div>
+            </div>
+          </div>
+        )
+      ) : null}
+    </Preview>
+  );
+}

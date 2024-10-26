276f452032e5da9e099baa3c87467a201230c112
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-10-26 03:19:16 -0300

    Refatorar campos de data e remover campos não utilizados no componente Input

--- src/components/RegisterForm/Input/index.js
+++ src/components/RegisterForm/Input/index.js
@@ -1,11 +1,28 @@
-import React, { useContext, useEffect, useRef } from 'react'
-import { useField } from '@unform/core'
-import { Asterisk } from 'lucide-react'
+import React, { useContext, useEffect, useRef } from "react";
+import { useField } from "@unform/core";
+import { Asterisk } from "lucide-react";
 
-const Input = ({ name, title, grow, shrink, defaultValueDDI = null, workloadUpdateName = false, readOnly = false, type, isZipCode = false, onlyUpperCase = false, onlyLowerCase = false, onlyInt = false, onlyFloat = false, isPhoneNumber = false, InputContext = null, ...rest }) => {
-  const inputRef = useRef()
-  const { fieldName, registerField, error } = useField(name)
-  const { disabled, required } = { ...rest }
+const Input = ({
+  name,
+  title,
+  grow,
+  shrink,
+  defaultValueDDI = null,
+  workloadUpdateName = false,
+  readOnly = false,
+  type,
+  isZipCode = false,
+  onlyUpperCase = false,
+  onlyLowerCase = false,
+  onlyInt = false,
+  onlyFloat = false,
+  isPhoneNumber = false,
+  InputContext = null,
+  ...rest
+}) => {
+  const inputRef = useRef();
+  const { fieldName, registerField, error } = useField(name);
+  const { disabled, required } = { ...rest };
 
   function maskZipCode(input) {
     let output = "";
@@ -13,7 +30,7 @@ const Input = ({ name, title, grow, shrink, defaultValueDDI = null, workloadUpda
       if (g1.length) {
         output += g1;
       }
-    })
+    });
 
     return output;
   }
@@ -22,36 +39,36 @@ const Input = ({ name, title, grow, shrink, defaultValueDDI = null, workloadUpda
     registerField({
       name: fieldName,
       ref: inputRef,
-      getValue: ref => {
-        return ref.current.value
+      getValue: (ref) => {
+        return ref.current.value;
       },
       setValue: (ref, value) => {
         ref.current.value = value;
       },
-      clearValue: ref => {
-        ref.current.value = ''
+      clearValue: (ref) => {
+        ref.current.value = "";
       },
-    })
-  }, [fieldName, registerField])
+    });
+  }, [fieldName, registerField]);
 
-  const { generalForm, setSuccessfullyUpdated } = useContext(InputContext)
+  const { generalForm, setSuccessfullyUpdated } = useContext(InputContext);
 
   function handleChanged() {
     if (onlyUpperCase) {
       const value = generalForm.current.getFieldValue(name);
-      generalForm.current.setFieldValue(name, value.toUpperCase())
+      generalForm.current.setFieldValue(name, value.toUpperCase());
     }
     if (onlyLowerCase) {
       const value = generalForm.current.getFieldValue(name);
-      generalForm.current.setFieldValue(name, value.toLowerCase())
+      generalForm.current.setFieldValue(name, value.toLowerCase());
     }
     if (onlyInt) {
       const value = generalForm.current.getFieldValue(name);
-      generalForm.current.setFieldValue(name, value.replace(/\D/g, ""))
+      generalForm.current.setFieldValue(name, value.replace(/\D/g, ""));
     }
     if (onlyFloat) {
       const value = generalForm.current.getFieldValue(name);
-      generalForm.current.setFieldValue(name, value.match(/^[0-9]*\.?[0-9]*$/))
+      generalForm.current.setFieldValue(name, value.match(/^[0-9]*\.?[0-9]*$/));
     }
     if (isPhoneNumber) {
       // const value = generalForm.current.getFieldValue(name);
@@ -62,19 +79,35 @@ const Input = ({ name, title, grow, shrink, defaultValueDDI = null, workloadUpda
       // generalForm.current.setFieldValue(name, maskZipCode(value))
     }
     if (workloadUpdateName) {
-      const days_per_week = generalForm.current.getFieldValue('days_per_week');
-      const hours_per_day = generalForm.current.getFieldValue('hours_per_day');
-      generalForm.current.setFieldValue('name', `${days_per_week.toString()} day(s) per week, ${hours_per_day.toString()} hour(s) per day.`)
+      const days_per_week = generalForm.current.getFieldValue("days_per_week");
+      const hours_per_day = generalForm.current.getFieldValue("hours_per_day");
+      generalForm.current.setFieldValue(
+        "name",
+        `${days_per_week.toString()} day(s) per week, ${hours_per_day.toString()} hour(s) per day.`
+      );
     }
 
-    setSuccessfullyUpdated(false)
+    setSuccessfullyUpdated(false);
   }
-  const width = shrink ? 'w-full md:w-auto max-w-32' : 'w-full md:w-auto'
+  const width = shrink ? "w-full md:w-auto max-w-32" : "w-full md:w-auto";
   return (
-    <div className={`${type === 'hidden' ? 'hidden' : 'flex'} flex-col justify-center items-start relative ${width} ${grow ? 'grow' : ''}`}>
-      <div className='px-1 text-xs flex flex-row justify-between items-center'>{title} {required && <Asterisk color='#e00' size={12} />}</div>
-      <div className='w-full flex flex-row justify-center items-center'>
-        <div htmlFor={name} className={`flex-1 border rounded-sm p-2 px-2 text-sm flex flex-row justify-between items-center gap-2 ${(disabled || readOnly) && 'bg-gray-100'} ${error && 'border-red-300'}`}>
+    <div
+      className={`${
+        type === "hidden" ? "hidden" : "flex"
+      } flex-col justify-center items-start relative ${width} ${
+        grow ? "grow" : ""
+      }`}
+    >
+      <div className="px-1 text-xs flex flex-row justify-between items-center">
+        {title} {required && <Asterisk color="#e00" size={12} />}
+      </div>
+      <div className="w-full flex flex-row justify-center items-center">
+        <div
+          htmlFor={name}
+          className={`flex-1 border rounded-sm p-2 px-2 text-sm flex flex-row justify-between items-center gap-2 ${
+            (disabled || readOnly) && "bg-gray-100"
+          } ${error && "border-red-300"}`}
+        >
           <input
             id={name}
             name={name}
@@ -83,14 +116,13 @@ const Input = ({ name, title, grow, shrink, defaultValueDDI = null, workloadUpda
             type={type}
             readOnly={readOnly}
             {...rest}
-            className='text-sm focus:outline-none flex-1 bg-transparent w-full'
+            className="text-sm focus:outline-none flex-1 bg-transparent w-full"
           />
         </div>
-
       </div>
       {/* {error && <span className={`text-xs text-red-500 absolute top-7 bg-white px-2 rounded-full right-4`}>{error}</span>} */}
-    </div >
-  )
-}
+    </div>
+  );
+};
 
-export default Input
+export default Input;

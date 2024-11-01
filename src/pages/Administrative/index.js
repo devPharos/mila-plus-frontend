bdb1f66cfe29207790c335f44ba01e4c1cd3f28c
Flavio Gabriel <flaviogabrielsr0507@gmail.com>
2024-11-01 02:59:37 -0300

    Refatorar página administrativa e adicionar novo componente Sidebar

--- src/pages/Administrative/index.js
+++ src/pages/Administrative/index.js
@@ -1,22 +1,30 @@
-import React, { useContext, useEffect } from 'react';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
-import Sidebar from '../Sidebar';
-import { PageContext } from '~/App';
+import React, { useContext, useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import Sidebar from "../Sidebar";
+import { PageContext } from "~/App";
 
 export default function Administrative() {
-  const { pathname } = useLocation()
+  const { pathname } = useLocation();
   const navigate = useNavigate();
 
-  const { pages } = useContext(PageContext)
+  const { pages } = useContext(PageContext);
 
   useEffect(() => {
-    if (pathname.toUpperCase() === '/Administrative'.toUpperCase() || pathname.toUpperCase() === '/Administrative/'.toUpperCase()) {
-      navigate("/Administrative/Calendar")
+    if (
+      pathname.toUpperCase() === "/Administrative".toUpperCase() ||
+      pathname.toUpperCase() === "/Administrative/".toUpperCase()
+    ) {
+      navigate("/Administrative/Calendar");
     }
-  }, [pathname])
+  }, [pathname]);
 
-  return <div className='w-full bg-gradient-to-br from-gray-300 via-indigo-300 to-mila_orange flex flex-1 flex-row justify-between items-center px-4 pt-8 shadow-lg overflow-y-scroll'>
-    <Sidebar main='administrative' pages={pages.find(page => page.name === 'Administrative').children} />
-    <Outlet />
-  </div>
+  return (
+    <div className="w-full bg-gradient-to-br from-gray-300 via-indigo-300 to-mila_orange flex flex-1 flex-row justify-between items-center px-4 pt-8 shadow-lg overflow-y-scroll">
+      <Sidebar
+        main="administrative"
+        pages={pages.find((page) => page.name === "Administrative").children}
+      />
+      <Outlet />
+    </div>
+  );
 }

503157ad8b6aa6686a347c4a5ddaf8208f427aed
PharosIT <admin@pharosit.com.br>
2025-03-04 17:53:25 -0300

    recurrence filter fix

--- src/pages/Academic/index.js
+++ src/pages/Academic/index.js
@@ -38,7 +38,7 @@ export default function Academic() {
 
       return;
     } else {
-      if (value) {
+      if (value === false || value) {
         setActiveFilters([
           ...activeFilters.filter((el) => el.title != title),
           { title, value },

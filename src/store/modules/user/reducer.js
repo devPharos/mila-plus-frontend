e4e2e7a634c4133284dc6d55f56b064d96cc43ba
João Darwin <joaodarwin.ip22@gmail.com>
2025-09-19 01:29:05 -0300

    feat: Criar redux module to manage profile changes

--- src/store/modules/user/reducer.js
+++ src/store/modules/user/reducer.js
@@ -1,30 +1,20 @@
-import { produce } from 'immer';
-
 const INITIAL_STATE = {
-  filial: {
-    code: '',
-    name: '',
-    active: true
-  },
-  profile: null,
-}
+    profile: null,
+    loading: false,
+};
 
 export default function user(state = INITIAL_STATE, action) {
-  return produce(state, draft => {
     switch (action.type) {
-      case '@auth/LOGIN_SUCCESS': {
-        draft.profile = action.payload.user;
-        break;
-      }
-      case '@user/UPDATE_PROFILE_SUCCESS': {
-        draft.profile = action.payload.profile;
-        break;
-      }
-      case '@auth/LOGOUT': {
-        draft.profile = null;
-        break;
-      }
-      default:
+        case "@user/UPDATE_PROFILE_REQUEST": {
+            return { ...state, loading: true };
+        }
+        case "@user/UPDATE_PROFILE_SUCCESS": {
+            return { ...state, profile: action.payload.profile, loading: false };
+        }
+        case "@user/UPDATE_PROFILE_FAILURE": {
+            return { ...state, loading: false };
+        }
+        default:
+            return state;
     }
-  });
 }
--- src/store/modules/user/sagas.js
+++ src/store/modules/user/sagas.js
@@ -1,69 +1,33 @@
 import { takeLatest, call, put, all } from "redux-saga/effects";
-// import { toast } from "react-toastify";
-
+import { toast } from "react-toastify";
 import api from "~/services/api";
+import {
+    updateProfileSuccess,
+    updateProfileFailure,
+} from "./actions";
 
-import { updateProfileSuccess, updateProfileFailure } from "./actions";
+function* updateProfile({ payload }) {
+    try {
+        const { data } = payload;
 
-export function* updateProfile({ payload }) {
-  const {
-    nome,
-    email,
-    avatar_id,
-    language,
-    id,
-    oldEmail,
-    filialDefault,
-    filial,
-    ...rest
-  } = payload.data;
-  let data = {};
+        const response = yield call(api.patch, "/users/me", data);
 
-  try {
-    if (avatar_id && avatar_id !== "undefined") {
-      const { data2 } = yield call(api.get, `s3files/${avatar_id}`);
-      data = data2;
+        toast.success("Profile updated with success!");
+        yield put(updateProfileSuccess(response.data));
+    } catch (err) {
+        switch (err.response.data.error) {
+            case "email-already-used":
+                toast.error("Email is already used.")
+                break;
+            case "user-does-not-exist":
+                toast.error("User does not exist.")
+                break;
+            default:
+                toast.error("Error on try update your profile. Try again.");
+        }
+
+        yield put(updateProfileFailure());
     }
-
-    const profile = Object.assign(
-      {
-        nome,
-        email,
-        avatar_id,
-        language,
-        id,
-        oldEmail,
-        avatar: data,
-        filialDefault,
-        filial
-      },
-      rest.oldPassword ? rest : {}
-    );
-
-    const response = yield call(api.put, "users", profile);
-
-    // toast.success(lang.profile.p9);
-
-    yield put(updateProfileSuccess(response.data));
-  } catch (err) {
-    switch (err.response.data.error) {
-      case "email-already-used":
-        // toast.error(lang.errors.e2, { className: "error" });
-        break;
-      case "user-does-not-exist":
-        // toast.error(lang.errors.e3, { className: "error" });
-        break;
-      case "passwords-do-not-match":
-        // toast.error(lang.errors.e4, { className: "error" });
-        break;
-      case "wrong-password":
-        // toast.error(lang.errors.e5, { className: "error" });
-        break;
-      default:
-      // toast.error(lang.errors.e1, { className: "error" });
-    }
-    yield put(updateProfileFailure());
-  }
 }
 
 export default all([takeLatest("@user/UPDATE_PROFILE_REQUEST", updateProfile)]);
